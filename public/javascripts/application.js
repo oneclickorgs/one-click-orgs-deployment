f9588ece671382eefae059c8a43dd80031df50f8
Chris Mear <chris@feedmechocolate.com>
2011-01-23 17:44:32 +0000

    First pass of T&Cs confirmation for founder on New Organisation page.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -32,24 +32,8 @@ $(document).ready(function () {
   $('#button-constitution-change').click(function() {
     $(location).attr('href','/settings');
   });
-  
-  // Setup a new org, autofill subdomain while typing group name
-  // Hide helper text:
-  $('body.setup p#help_subdomain').hide();
-  // Show helper text on typing:
-  $('body.setup input#organisation_name').focus(function() {
-    $('body.setup p#help_subdomain').fadeIn('slow');
-  });
-  // On every key press:
-  $('body.setup input#organisation_name').keyup(function() {
-    var text = $('input#organisation_name').val();
-    text = text.toLowerCase();
-    // Regex to whitelist organisation name to generate a subdomain
-    // Obviously, we still need to validate this in the app
-    text = text.replace(/[^a-z0-9]/g,'');
-    $('input#organisation_subdomain').val(text);
-  });
 });
+
 var OneClickOrgs = {};
 
 OneClickOrgs.activateLightbox = function () {
