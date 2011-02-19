67b126f38528076820defdf50d9c1c4d25c416eb
Chris Mear <chris@feedmechocolate.com>
2011-02-19 22:43:53 +0000

    Set all members to 'Member' class when enacting FoundOrganisationProposal. Closes #233.
    Need to set even those members who voted against the FoundOrganisationProposal, in case they get re-invited to the organisation at a later date.

--- spec/models/found_organisation_proposal_spec.rb
+++ spec/models/found_organisation_proposal_spec.rb
@@ -30,4 +30,50 @@ describe FoundOrganisationProposal do
       @proposal.save.should be_true
     end
   end
+  
+  describe "enactment" do
+    before(:each) do
+      # Set up mock organisation and member classes
+      @organisation = mock_model(Organisation, :active! => nil, :save => false)
+      
+      @founder_class = mock_model(MemberClass, :name => 'Founder', :description => nil)
+      @founding_member_class = mock_model(MemberClass, :name => 'Founding Member', :description => nil)
+      @member_class = mock_model(MemberClass, :name => 'Member', :description => nil)
+      
+      @organisation.stub!(:member_classes).and_return(@member_classes_association = mock('member classes association'))
+      @member_classes_association.stub!(:find_by_name).with('Founder').and_return(@founder_class)
+      @member_classes_association.stub!(:find_by_name).with('Founding Member').and_return(@founding_member_class)
+      @member_classes_association.stub!(:find_by_name).with('Member').and_return(@member_class)
+      
+      # Mock up a founder and three founding members
+      @members = [
+        mock_model(Member, :member_class => @founder_class,         :member_class= => nil, :save! => true),
+        mock_model(Member, :member_class => @founding_member_class, :member_class= => nil, :save! => true),
+        mock_model(Member, :member_class => @founding_member_class, :member_class= => nil, :save! => true),
+        mock_model(Member, :member_class => @founding_member_class, :member_class= => nil, :save! => true, :eject! => true)
+      ]
+      @organisation.stub!(:members).and_return(@members)
+      
+      # Mock that founder and first two founding members vote for the founding,
+      # and that the final founding member votes against
+      @votes = [
+        mock_model(Vote, :member_id => @members[0].id, :for? => true),
+        mock_model(Vote, :member_id => @members[1].id, :for? => true),
+        mock_model(Vote, :member_id => @members[2].id, :for? => true),
+        mock_model(Vote, :member_id => @members[3].id, :for? => false)
+      ]
+      
+      @proposal = FoundOrganisationProposal.new(:proposer => @members[0], :organisation => @organisation)
+      @proposal.stub!(:votes).and_return(@votes)
+    end
+    
+    it "sets the member class of all existing members to 'Member'" do
+      @members.each do |member|
+        member.should_receive(:member_class=).with(@member_class).ordered
+        member.should_receive(:save!).ordered
+      end
+      
+      @proposal.enact!
+    end
+  end
 end
