5ae4ef6c29de8e58595122100e7d1282e41fb068
Chris Mear <chris@feedmechocolate.com>
2011-03-17 12:19:05 +0000

    Add configurable Google Analytics code. Closes #188.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,26 @@ module ApplicationHelper
     EOC
   end
   
+  def google_analytics_code
+    return unless Rails.env.production? && OneClickOrgs::GoogleAnalytics.active?
+    raw <<-EOC
+      <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', '#{OneClickOrgs::GoogleAnalytics.id}']);
+        _gaq.push(['_setDomainName', '#{OneClickOrgs::GoogleAnalytics.domain}']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+      </script>
+    EOC
+  end
+  
   def error_messages_for(object)
     messages = object.errors.full_messages.map do |message|
       content_tag(:li, message)
--- lib/one_click_orgs/version.rb
+++ lib/one_click_orgs/version.rb
@@ -1,5 +1,5 @@
 module OneClickOrgs
-  VERSION = "1.0.0rc2"
+  VERSION = "1.0.0rc2" unless defined?(VERSION)
   
   def self.version
     if VERSION =~ /^0/
