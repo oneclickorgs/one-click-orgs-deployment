0ffb98e29c907d7018b1f2e0cede8aefd90f8b07
Chris Mear <chris@feedmechocolate.com>
2011-04-30 17:52:38 +0100

    Update version for v1.1.0 prep.

--- lib/one_click_orgs/version.rb
+++ lib/one_click_orgs/version.rb
@@ -1,5 +1,5 @@
 module OneClickOrgs
-  VERSION = "1.0.1" unless defined?(::OneClickOrgs::VERSION)
+  VERSION = "1.1.0alpha" unless defined?(::OneClickOrgs::VERSION)
   
   def self.version
     if VERSION =~ /^0/
