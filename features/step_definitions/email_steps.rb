f066b19dfedf1d80f77f6c8062c8960f86e27107
Chris Mear <chris@feedmechocolate.com>
2011-05-07 00:35:05 +0100

    Add feature: Accepting a founding member invitation.

--- features/step_definitions/email_steps.rb
+++ features/step_definitions/email_steps.rb
@@ -1,11 +1,31 @@
+require 'uri'
+
+def last_email
+  ActionMailer::Base.deliveries.last
+end
+
+Given /^I have received an email inviting me to become a founding member$/ do
+  Given "I have been invited to become a founding member"
+  @email = last_email
+end
+
+When /^I follow the invitation link in the email$/ do
+  @email ||= last_email
+  uri = URI.parse(@email.body.match(/ (http:\/\/.*?) /)[1])
+  subdomain = uri.host.split('.')[0]
+  path = uri.path
+  Given %Q{the subdomain is "#{subdomain}"}
+  visit(path)
+end
+
 Then /^I should receive a welcome email$/ do
-  email = ActionMailer::Base.deliveries.last
-  email.to.should == [(@user ||= Member.last).email]
-  email.body.should =~ /created a draft constitution/
+  @email = last_email
+  @email.to.should == [(@user ||= Member.last).email]
+  @email.body.should =~ /created a draft constitution/
 end
 
 Then /^a founding member invitation email should be sent to "([^"]*)"$/ do |email_address|
-  email = ActionMailer::Base.deliveries.last
-  email.to.should == [email_address]
-  email.body.should =~ /You've been invited by .* to become a founding member/
+  @email = last_email
+  @email.to.should == [email_address]
+  @email.body.should =~ /You've been invited by .* to become a founding member/
 end
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,10 @@
 Given /^I am a founding member$/ do
   @user = @organisation.members.make(:pending, :member_class => @organisation.member_classes.find_by_name("Founding Member"))
 end
+
+Given /^I have been invited to become a founding member$/ do
+  @organisation.members.make(:pending,
+    :member_class => @organisation.member_classes.find_by_name("Founding Member"),
+    :send_welcome => true
+  )
+end
