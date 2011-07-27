e0cc70c761e02c2e4a85b9d5fe5185cc9f3f5471
Chris Mear <chris@feedmechocolate.com>
2011-07-27 14:17:07 +0100

    Add test for existing feature: Commenting on a proposal.

---
+++ features/step_definitions/comment_steps.rb
@@ -0,0 +1,8 @@
+When /^I enter a comment of "([^"]*)"$/ do |comment_body|
+  fill_in('comment[body]', :with => comment_body)
+end
+
+Then /^I should see a comment by me saying "([^"]*)"$/ do |comment_body|
+  page.should have_css('.comment p.attribution a', :text => @user.name)
+  page.should have_css('.comment p', :text => comment_body)
+end
