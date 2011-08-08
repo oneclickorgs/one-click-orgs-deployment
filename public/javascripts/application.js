875803ee5dc8e89437d1e1134cc55a515036f471
Chris Mear <chris@feedmechocolate.com>
2011-08-08 15:07:12 +0100

    Remove old constitution page JavaScript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -47,16 +47,6 @@ OneClickOrgs.trackAnalyticsEvent = function (eventName) {
 }
 
 $(document).ready(function () {
-  $('.constitution_proposal .reveal_link a').click(function (event) {
-    $(event.target).parents('.constitution_proposal').children('.proposal_form').slideDown('normal');
-    $(event.target).parents('.reveal_link').slideUp();
-    event.preventDefault();
-  });
-  $('.constitution_proposal .cancel_link').click(function (event) {
-    $(event.target).parents('.constitution_proposal').children('.reveal_link').slideDown('normal');
-    $(event.target).parents('.constitution_proposal').children('.proposal_form').slideUp();
-    event.preventDefault();
-  });
 
 
   // Forms on the Voting & proposals page
