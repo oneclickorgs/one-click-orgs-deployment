oneclickorgs/one-click-orgs-deployment

commit 1c3c2c84aa9e8a643754a9f0bce37feabcf1934e

Issue #227: fix founding vote status notifications
Member#has_seen notifications now checks the timestamp
AND the message passed in, to show relevant
messages for multiple attempts at forming
an organisation

app/controllers/application_controller.rb
@@ -144,9 +144,9 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  def show_notification_once(notification)
+  def show_notification_once(notification, created_at = '')
     return unless current_user
-    return if current_user.has_seen_notification?(notification)
+    return if current_user.has_seen_notification?(notification, created_at)
     show_notification(notification)
   end
   

app/models/member.rb
@@ -209,8 +209,8 @@ class Member < ActiveRecord::Base
   
   has_many :seen_notifications
   
-  def has_seen_notification?(notification)
-    seen_notifications.exists?(:notification => notification)
+  def has_seen_notification?(notification, created_at = '')
+    seen_notifications.exists?(:notification => notification, :created_at => created_at)
   end
   
   def has_seen_notification!(notification)

features/organisation_founding/show_failed_proposal.feature
@@ -13,12 +13,9 @@ Feature:
     And everyone has voted against the founding
     And the proposal closer runs
     And I have logged in
-    # Then show me the page
     Then I should see "Sorry, the Founding Vote has failed."
-    And another founding vote has been started
+    When another founding vote has been started
     And everyone has voted against the founding
     And the proposal closer runs
     And I go to the home page
-    Then show me the page
     Then I should see "Sorry, the Founding Vote has failed."
-    # Then I should see "Sorry, the founding vote has failed"

commit 75b593e478f5855861e6304174b8e14470aa5861

Fix that show_notification_once was showing notifications repeatedly. Closes #327.
Bug introduced in 1c3c2c84aa9e8a643754a9f0bce37feabcf1934e.

app/controllers/application_controller.rb
@@ -127,14 +127,11 @@ class ApplicationController < ActionController::Base
     if co.pending? && current_user.member_class.name == "Founder"
       show_notification_once(:convener_welcome)
     end
-
+    
     fop = co.found_organisation_proposals.last
-    # if the organisation is pending
-    # and the voting is finished (fop.closed)
-    # and a founding proposal exists
-    # and is the proposal
+    
     if co.pending? && fop && fop.closed? && !fop.accepted?
-      show_notification_once(:founding_proposal_failed)
+      show_notification_once(:founding_proposal_failed, fop.close_date)
     end
     
     # Only display founding_proposal_passed notification to
@@ -144,19 +141,26 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  # Show a notification once when a user is signed in, to notify of
-  # a specific event happening
+  # Show a one-off notification to a user, to notify of
+  # a specific event happening.
+  # 
+  # With #show_notification_once, a notification of a given type (e.g. a 'convener_welcome' notification) will only be shown once
+  # to a given user. If you want to show the notification regardless of whether the user has already seen it before,
+  # use #show_notification instead.
+  # 
+  # As an exception to this, you can pass the ignore_earlier_than parameter. If this user has seen this type of notification
+  # earlier than the timestamp you pass, the notification will be shown again.
   #
   # @param [Symbol] notification the notification type, `:founding_proposal_passed` or `:founding_proposal_failed`
-  # @param [optional, Timestamp] created_at the timestamp for that kind of notification, i.e `2011-06-04 13:14:37`
+  # @param [optional, Timestamp] ignore seen-notifications earlier than this time
   # @return [String] notification the string relating to the kind of notification `founding_proposal_passed`.
   #
   # @example Show a notification a user once that their proposal failed, allowing us to render the partial 'founding_proposal_failed' in the view
   #   show_notification_once(:founding_proposal_failed)
   #
-  def show_notification_once(notification, created_at = '')
+  def show_notification_once(notification, ignore_earlier_than = nil)
     return unless current_user
-    return if current_user.has_seen_notification?(notification, created_at)
+    return if current_user.has_seen_notification?(notification, ignore_earlier_than)
     show_notification(notification)
   end
   

app/models/member.rb
@@ -211,17 +211,15 @@ class Member < ActiveRecord::Base
   
   # Check if this notification has been shown to user already.
   #
-  # There is an assumption that a user will never require more than one of the
-  # same kind of notification, at the same time as another
-  #
   # @param [Symbol] notification the kind of notification
-  # @param [optional, Timestamp] created_at when the notification was created
-  #
-  # @example Check that a user saw that her latest proposal in an organisation failed at a particular time in June
-  #   current_user.has_seen_notification?(:founding_proposal_failed, "2011-06-04 13:14:37")
-  #
-  def has_seen_notification?(notification, created_at = '')
-    seen_notifications.exists?(:notification => notification, :created_at => created_at)
+  # @param [optional, Timestamp] ignore_earlier_than If you pass this timestamp, we only consider the period
+  # after the timestamp when checking to see if the member has already seen this notification.
+  def has_seen_notification?(notification, ignore_earlier_than = nil)
+    if ignore_earlier_than
+      seen_notifications.exists?(["notification = ? AND created_at >= ?", notification, ignore_earlier_than])
+    else
+      seen_notifications.exists?(:notification => notification)
+    end
   end
   
   def has_seen_notification!(notification)

features/notifications/show_notification_once.feature
@@ -1,0 +1,9 @@
+Feature: Show notification once
+  Notifications like the 'convener_welcome' should only be shown once.
+  
+  Scenario: Convener welcome is only shown once
+    Given the application is set up
+    When I create an organisation
+    Then I should see "Now you can read the constitution"
+    When I go to the members page
+    Then I should not see "Now you can read the constitution"

features/step_definitions/organisation_steps.rb
@@ -48,6 +48,10 @@ Given /^the organisation's name is "([^"]*)"$/ do |new_organisation_name|
   @organisation.clauses.set_text!(:organisation_name, new_organisation_name)
 end
 
+When /^I create an organisation$/ do
+  Given "I have created an organisation"
+end
+
 Then /^the organisation should be active$/ do
   @organisation.should be_active
 end