ccbad5303eb410a5e3f152df02a391b72489229d
Chris Mear <chris@feedmechocolate.com>
2011-08-16 13:47:38 +0100

    Ensure organisation name is properly escaped when used to form the 'From:' field in an email.
    Security issue reported by Darren McDonald.

---
+++ spec/mailers/oco_mailer_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe OcoMailer do
+  
+  class SpecMailer < OcoMailer
+    def notification(organisation_name="One Click Orgs")
+      create_mail(organisation_name, 'bob@example.com', "Example subject")
+    end
+  end
+  
+  it "quotes and escapes the 'From:' display name if necessary" do
+    mail = SpecMailer.notification('Bad Company Name"')
+    mail['From'].to_s.should == '"Bad Company Name\\"" <notifications@oneclickorgs.com>'
+  end
+  
+end
