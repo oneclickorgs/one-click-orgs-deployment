f03b78b64b30891eed641b388af452682b53fdb7
Chris Mear <chris@feedmechocolate.com>
2011-08-18 23:04:00 +0100

    Only show notification once for each (second and subsequent) failed FoundOrganisationProposal. Closes #330.

--- app/models/member.rb
+++ app/models/member.rb
@@ -223,14 +223,17 @@ class Member < ActiveRecord::Base
   # after the timestamp when checking to see if the member has already seen this notification.
   def has_seen_notification?(notification, ignore_earlier_than = nil)
     if ignore_earlier_than
-      seen_notifications.exists?(["notification = ? AND created_at >= ?", notification, ignore_earlier_than])
+      seen_notifications.exists?(["notification = ? AND updated_at >= ?", notification, ignore_earlier_than])
     else
       seen_notifications.exists?(:notification => notification)
     end
   end
   
   def has_seen_notification!(notification)
-    unless has_seen_notification?(notification)
+    seen_notification = seen_notifications.find_by_notification(notification)
+    if seen_notification
+      seen_notification.touch
+    else
       seen_notifications.create(:notification => notification)
     end
   end
