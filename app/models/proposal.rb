fd340931754aae04bb1f065458ccf7bd6273d8fd
Chris Mear <chris@feedmechocolate.com>
2012-02-20 11:41:41 +0000

    Reorganise the remainder of Proposal.

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -14,34 +14,72 @@ class Proposal < ActiveRecord::Base
   end
   
   belongs_to :organisation
+  belongs_to :proposer, :class_name => 'Member', :foreign_key => 'proposer_member_id'
   
   has_many :votes
   has_one :decision
-  
-  belongs_to :proposer, :class_name => 'Member', :foreign_key => 'proposer_member_id'
-  
   has_many :comments, :as => :commentable
   
   validates_presence_of :proposer_member_id
+  
+  scope :currently_open, lambda {where(["state = 'open' AND close_date > ?", Time.now.utc])}
+  scope :failed, lambda {where(["close_date < ? AND state = 'rejected'", Time.now.utc]).order('close_date DESC')}
+  
+  attr_accessor :force_passed
+  
+  # FINDERS
+  
+  def self.find_closeable_early_proposals
+    currently_open.all.select { |p| p.voting_system.can_be_closed_early?(p) }
+  end
+  
+  def self.close_early_proposals
+    find_closeable_early_proposals.each { |p| p.close! }
+  end
+  
+  # CLASS METHODS
 
+  def self.close_due_proposals
+    where(["close_date < ? AND state = 'open'", Time.now.utc]).all.each { |p| p.close! }
+  end
+  
+  def self.close_early_proposals
+    find_closeable_early_proposals.each { |p| p.close! }
+  end
+  
+  # Called every 60 seconds in the worker process (set up at end of file)
+  def self.close_proposals
+    close_due_proposals
+    close_early_proposals
+  end
+  
+  # GETTERS
+  
   def voting_period
     organisation.constitution.voting_period
   end
   
-  # Should the proposal automatically create a support vote by the
-  # proposer when the proposal is started?
-  def automatic_proposer_support_vote?
-    true
-  end
-  
-  after_create :cast_support_vote_by_proposer
-  def cast_support_vote_by_proposer
-    proposer.cast_vote(:for, self) if automatic_proposer_support_vote?
-  end
- 
   def end_date
     self.close_date
   end
+  
+  def accepted_or_rejected
+    accepted? ? "accepted" : "rejected"
+  end
+  
+  def closed?
+    !self.open?
+  end
+  
+  def voting_system
+    organisation.constitution.voting_system(:general)
+  end
+
+  def passed?
+    @force_passed || voting_system.passed?(self)
+  end
+  
+  # ASSOCIATION HELPERS
   
   # Returns a Vote by the member specified, or Nil
   def vote_by(member)
@@ -61,6 +99,8 @@ class Proposal < ActiveRecord::Base
     organisation.member_count_for_proposal(self)
   end
   
+  # CALCULATIONS
+  
   def abstained
     member_count - total_votes
   end
@@ -69,62 +109,42 @@ class Proposal < ActiveRecord::Base
     votes_for + votes_against
   end
   
-  def after_reject
-  end
-  
-  def accepted_or_rejected
-    accepted? ? "accepted" : "rejected"
-  end
-  
-  def enact!
-  end
-  
-  def closed?
-    !self.open?
-  end
-  
-  def voting_system
-    organisation.constitution.voting_system(:general)
-  end
-
-  def passed?
-    @force_passed || voting_system.passed?(self)
-  end
-  
-  def self.find_closeable_early_proposals
-    currently_open.all.select { |p| p.voting_system.can_be_closed_early?(p) }
-  end
-
-  def self.close_due_proposals
-    where(["close_date < ? AND state = 'open'", Time.now.utc]).all.each { |p| p.close! }
-  end
-  
-  def self.close_early_proposals
-    find_closeable_early_proposals.each { |p| p.close! }
-  end
-  
-  # Called every 60 seconds in the worker process (set up at end of file)
-  def self.close_proposals
-    close_due_proposals
-    close_early_proposals
-  end
-  
-  scope :currently_open, lambda {where(["state = 'open' AND close_date > ?", Time.now.utc])}
-  
-  scope :failed, lambda {where(["close_date < ? AND state = 'rejected'", Time.now.utc]).order('close_date DESC')}
-  
-  def to_event
-    {:timestamp => self.creation_date, :object => self, :kind => (closed? && !accepted?) ? :failed_proposal : :proposal }
-  end
-
   def duration
     creation_date && end_date && (end_date - creation_date)
   end
   
-  def decision_notification_message
-    nil
+  # BEHAVIOUR REDEFINED BY SUBCLASSES
+  
+  def enact!
   end
   
+  def after_reject
+  end
+  
+  # Should the proposal automatically create a support vote by the
+  # proposer when the proposal is started?
+  def automatic_proposer_support_vote?
+    true
+  end
+  
+  def decision_notification_message
+  end
+  
+  # CALLBACKS
+  
+  after_create :cast_support_vote_by_proposer
+  def cast_support_vote_by_proposer
+    proposer.cast_vote(:for, self) if automatic_proposer_support_vote?
+  end
+  
+  # TIMELINE
+  
+  def to_event
+    {:timestamp => self.creation_date, :object => self, :kind => (closed? && !accepted?) ? :failed_proposal : :proposal }
+  end
+  
+  # MISCELLANEOUS
+
   # This works around a bug in state_machine which means we cannot specify #create_decision
   # directly in the after_transition callback. If we do, ActiveRecord attempts to use the
   # Transition object as attributes to the new Decision object, resulting ultimately in an
