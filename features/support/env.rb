13d5bbd1150990a0f23c237509c53a68d12f8fe7
Chris Mear <chris@feedmechocolate.com>
2012-04-07 15:42:56 +0100

    Syntax fix.

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,7 +16,7 @@ Capybara.default_selector = :css
 Capybara.register_driver :selenium_with_long_timeout do |app|
   client = Selenium::WebDriver::Remote::Http::Default.new
   client.timeout = 120
-  Capybara::Driver::Selenium.new(app, :browser => :firefox, :http_client => client)
+  Capybara::Selenium::Driver.new(app, :browser => :firefox, :http_client => client)
 end
 
 Capybara.javascript_driver = :selenium_with_long_timeout
