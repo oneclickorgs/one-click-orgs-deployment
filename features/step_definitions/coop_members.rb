d3e1f686026b0bf50946c086b98353c9590fee48
Chris Mear <chris@feedmechocolate.com>
2012-07-13 21:58:45 +0100

    Add pending step definitions for new co-op Background steps.

---
+++ features/step_definitions/coop_members.rb
@@ -0,0 +1,19 @@
+Given /^I am the founder of the draft co\-op$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am a founding member of the draft co\-op$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am the (S|s)ecretary of the co\-op$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am a (M|m)ember of the co\-op$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am a Director of the co\-op$/ do
+  pending # express the regexp above with the code you wish you had
+end
