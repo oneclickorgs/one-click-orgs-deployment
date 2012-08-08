1e202d7541a7e7391ef1b7aa2ef2f4ed7ebbf7b3
Chris Mear <chris@feedmechocolate.com>
2012-08-08 21:22:26 +0100

    Assign first co-op member an appropriate member class.

--- app/controllers/coops_controller.rb
+++ app/controllers/coops_controller.rb
@@ -1,23 +1,26 @@
 class CoopsController < ApplicationController
   skip_before_filter :ensure_organisation_exists
   skip_before_filter :ensure_authenticated
-  
+
   layout "setup"
-  
+
   def new
     @member = Member.new
     @coop = Coop.new
   end
-  
+
   def create
     @coop = Coop.new(params[:coop])
     @member = @coop.members.build(params[:member])
-    
+
     @coop.save
     @member.save
-    
+
+    @member.member_class = @coop.member_classes.find_by_name("Founder Member")
+    @member.save
+
     log_in(@member)
-    
+
     redirect_to root_url(host_and_port(@coop.host))
   end
 end
