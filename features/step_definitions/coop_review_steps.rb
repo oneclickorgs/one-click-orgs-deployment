d3c950fba2ffdd8e7fdc9279e40542382f38fe96
Chris Mear <chris@feedmechocolate.com>
2012-08-08 23:24:07 +0100

    Feature: Viewing submitted co-ops.

--- features/step_definitions/coop_review_steps.rb
+++ features/step_definitions/coop_review_steps.rb
@@ -2,6 +2,10 @@ Given /^a co\-op has been submitted$/ do
   @coop = @organisation = Coop.make!(:proposed)
 end
 
+Given /^some co\-ops have been submitted for registration$/ do
+  @coops = Coop.make!(2, :proposed)
+end
+
 When /^I press "(.*?)" for the co\-op$/ do |button|
   @coop ||= Coop.proposed.last
   within("#coop_#{@coop.id}") do
@@ -13,5 +17,15 @@ Then /^I should see that the co\-op is approved$/ do
   @coop ||= Coop.active.last
   within('.active_coops') do
     page.should have_content(@coop.name)
+  end
+end
+
+Then /^I should see a list of the submitted co\-ops$/ do
+  @coops ||= Coop.proposed
+
+  within('.proposed_coops') do
+    @coops.each do |coop|
+      page.should have_content(coop.name)
+    end
   end
 end
