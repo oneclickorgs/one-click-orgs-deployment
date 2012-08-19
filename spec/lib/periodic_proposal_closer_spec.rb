a0c63a584e0ca168face7b0ba066d0682bac52ef
Chris Mear <chris@feedmechocolate.com>
2012-08-19 23:22:34 +0100

    Add pending specs for new periodic closer behaviour.

---
+++ spec/lib/periodic_proposal_closer_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe PeriodicProposalCloser do
+
+  it "closes proposals that can be closed"
+  it "closes elections that can be closed"
+  it "has an appropriate name given that it now closes elections as well"
+
+end
