fe8cc0b19c695cef53347e61f26ec6f4fe0857be
Chris Mear <chris@feedmechocolate.com>
2012-10-09 14:16:01 +0100

    Fix whitespace.

--- app/controllers/board_resolutions_controller.rb
+++ app/controllers/board_resolutions_controller.rb
@@ -2,7 +2,7 @@ class BoardResolutionsController < ApplicationController
   def new
     @board_resolution = co.board_resolutions.build
   end
-  
+
   def create
     @board_resolution = co.board_resolutions.build(params[:board_resolution])
     @board_resolution.proposer = current_user
