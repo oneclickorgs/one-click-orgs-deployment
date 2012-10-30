c28251e4d6eb79fea60a9a92023de50b4bf3183b
Chris Mear <chris@feedmechocolate.com>
2012-10-30 11:20:24 +0000

    Adjust deploy recipe for eu-west EC2 instance.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,21 +5,22 @@ set :application, "one_click_orgs"
 set :repository,  "git://github.com/oneclickorgs/one-click-orgs-deployment"
 
 set :scm, :git
-set :user, 'oneclickorgs'
+set :user, 'ubuntu'
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa-oco")]
 set :use_sudo, false
-set :sv, "~/local/bin/sv"
-set :rake, "/home/oneclickorgs/local/bin/bundle exec rake"
-default_environment["PATH"] = "/home/oneclickorgs/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/games"
+# set :sv, "~/local/bin/sv"
+set :rake, "/usr/local/bin/bundle exec rake"
+# default_environment["PATH"] = "/home/oneclickorgs/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/games"
 
-role :web, "us1.okfn.org"                          # Your HTTP server, Apache/etc
-role :app, "us1.okfn.org"                          # This may be the same as your `Web` server
-role :db,  "us1.okfn.org", :primary => true # This is where Rails migrations will run
+role :web, "ec2-46-51-160-166.eu-west-1.compute.amazonaws.com"                          # Your HTTP server, Apache/etc
+role :app, "ec2-46-51-160-166.eu-west-1.compute.amazonaws.com"                          # This may be the same as your `Web` server
+role :db,  "ec2-46-51-160-166.eu-west-1.compute.amazonaws.com", :primary => true # This is where Rails migrations will run
 
-set :deploy_to, "/home/oneclickorgs/var/www/conference.coop.oneclickorgs.com"
+set :deploy_to, "/var/www/conference.oneclickorgs.com"
 set :branch,    "conference-coop-oneclickorgs-com"
 
 set :bundle_dir, File.join(fetch(:shared_path), 'bundler')
-set :bundle_cmd, "~/local/bin/bundle"
+set :bundle_cmd, "/usr/local/bin/bundle"
 set :bundle_roles, [:app]
 
 before 'deploy:assets:precompile' do
@@ -29,13 +30,13 @@ before 'deploy:assets:precompile' do
   END
 end
 
-after  "deploy:restart", "worker:restart"
+# after  "deploy:restart", "worker:restart"
 
-namespace :worker do
-  task :restart, :roles => :app do
-    run "for service in #{shared_path}/sv/*; do #{sv} restart $service || #{sv} kill $service; done"
-  end
-end
+# namespace :worker do
+#   task :restart, :roles => :app do
+#     run "for service in #{shared_path}/sv/*; do #{sv} restart $service || #{sv} kill $service; done"
+#   end
+# end
 
 namespace :deploy do
   task :start do ; end
