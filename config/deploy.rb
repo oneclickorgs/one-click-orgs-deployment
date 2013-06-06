d409b07e6aecbe55f811f33cefc9c8ecd0f112d3
Chris Mear <chris@feedmechocolate.com>
2013-06-06 21:09:32 +0100

    Deploy: Compile assets locally instead of on destination server.
    The asset precompilation process is so intensive that, when
    deploying to a small VM, it is not uncommon for it to be killed by
    the OS. Instead, use our local dev machine to do the heavy lifting.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require 'bundler/capistrano'
-load 'deploy/assets'
+# load 'deploy/assets'
 
 set :application, "one_click_orgs"
 set :repository,  "git://github.com/oneclickorgs/one-click-orgs-deployment"
@@ -45,5 +45,37 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+# PRECOMPILE ASSETS LOCALLY
+# FROM http://www.rostamizadeh.net/blog/2012/04/14/precompiling-assets-locally-for-capistrano-deployment
+
+before 'deploy:finalize_update', 'deploy:assets:symlink'
+after 'deploy:update_code', 'deploy:assets:precompile'
+
+namespace :deploy do
+  namespace :assets do
+
+    task :precompile, :roles => :web do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        logger.info "Skipping asset precompilation because there were no asset changes"
+      end
+    end
+
+    task :symlink, roles: :web do
+      run ("rm -rf #{latest_release}/public/assets &&
+            mkdir -p #{latest_release}/public &&
+            mkdir -p #{shared_path}/assets &&
+            ln -s #{shared_path}/assets #{latest_release}/public/assets")
+    end
   end
 end
