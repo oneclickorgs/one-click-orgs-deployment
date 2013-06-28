4aa937f39c2bd710055b7940ae41d7c776515d55
Chris Mear <chris@feedmechocolate.com>
2013-06-28 13:04:34 +0100

    Temporary workaround for Markdown-generated HTML being escaped.

--- config/initializers/haml_filters.rb
+++ config/initializers/haml_filters.rb
@@ -1,13 +1,13 @@
-require 'haml/filters'
-require 'rdiscount'
+# require 'haml/filters'
+# require 'rdiscount'
 
-module Haml
-  module Filters
-    # Override Haml's built-in Markdown filter to add escaping of raw HTML.
-    module Markdown
-      def render(text)
-        ::RDiscount.new(text, :filter_html).to_html
-      end
-    end
-  end
-end
+# module Haml
+#   module Filters
+#     # Override Haml's built-in Markdown filter to add escaping of raw HTML.
+#     module Markdown
+#       def render(text)
+#         ::RDiscount.new(text, :filter_html).to_html.html_safe
+#       end
+#     end
+#   end
+# end
