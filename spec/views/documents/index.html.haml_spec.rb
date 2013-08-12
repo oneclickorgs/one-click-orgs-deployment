4c77aa8b701ada066dd8d294ac7e917282235013
Chris Mear <chris@feedmechocolate.com>
2013-08-12 15:33:01 +0100

    Use documents#index as home page for all registration documents.

--- spec/views/documents/index.html.haml_spec.rb
+++ spec/views/documents/index.html.haml_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe 'documents/index' do
 
+  before(:each) do
+    allow(view).to receive(:co).and_return(mock_model(Coop, :name => "Acme"))
+  end
+
   it "renders a link to the letter of engagement document" do
     render
     expect(rendered).to have_selector(:a, :href => '/documents/letter_of_engagement')
