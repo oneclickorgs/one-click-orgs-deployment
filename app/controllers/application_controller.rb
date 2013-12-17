chrismear/one-click-orgs

Fix that :host parameter to url_for no longer accepts the port part of the URL in Rails 3.1.

app/controllers/application_controller.rb
@@ -202,7 +202,7 @@ protected
   
   def ensure_organisation_exists
     unless current_organisation
-      redirect_to(new_association_url(:host => Setting[:signup_domain]))
+      redirect_to(new_association_url(host_and_port(Setting[:signup_domain])))
     end
   end
   
@@ -234,6 +234,15 @@ protected
   
   def find_constitution
     @constitution = co.constitution
+  end
+  
+  def host_and_port(domain)
+    host, port = domain.split(':')
+    if port
+      {:host => host, :port => port}
+    else
+      {:host => host}
+    end
   end
   
   # EXCEPTION HANDLING

oneclickorgs/one-click-orgs-deployment

(cherry picked from commit 7ec5fe68daff5c53e65e2de73b504adeaa47a6a6)
Conflicts:
	app/controllers/application_controller.rb
	app/controllers/companies_controller.rb
	app/controllers/organisations_controller.rb

app/controllers/application_controller.rb
@@ -202,7 +202,7 @@ class ApplicationController < ActionController::Base
   
   def ensure_organisation_exists
     unless current_organisation
-      redirect_to(new_organisation_url(:host => Setting[:signup_domain]))
+      redirect_to(new_organisation_url(host_and_port(Setting[:signup_domain])))
     end
   end
   
@@ -239,8 +239,17 @@ class ApplicationController < ActionController::Base
     redirect_to(:controller => 'welcome', :action => 'index')
   end
   
+  def host_and_port(domain)
+    host, port = domain.split(':')
+    if port
+      {:host => host, :port => port}
+    else
+      {:host => host}
+    end
+  end
+
   # EXCEPTIONS
-  
+
   rescue_from NotFound, :with => :render_404
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   def render_404