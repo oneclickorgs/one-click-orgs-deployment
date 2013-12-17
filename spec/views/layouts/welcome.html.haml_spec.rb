ccd5d6fb2360eafcb8f350fcd76c5e9c6de57215
Chris Mear <chris@feedmechocolate.com>
2013-12-17 11:59:49 +0000

    Don't use deprecated feature of passing format in template name.

--- spec/views/layouts/welcome.html.haml_spec.rb
+++ spec/views/layouts/welcome.html.haml_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "layouts/welcome.html.haml" do
+describe "layouts/welcome" do
   context "when user is logged in" do
     before(:each) do
       @members_association = mock('members association', :first => @founder = mock_model(Member, :name => "Bob Smith"))
