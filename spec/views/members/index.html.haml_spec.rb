0757db814924fd84a959174508c82cb0d7698f49
Chris Mear <chris@feedmechocolate.com>
2014-01-01 14:35:04 +0000

    Fix that 'last login' didn't show year, only day and month. Closes #455.

---
+++ spec/views/members/index.html.haml_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe 'members/index' do
+
+  let(:members) {[mock_model(Member,
+    :last_logged_in_at => Date.new(1993, 1, 1),
+    :gravatar_url => nil,
+    :name => nil,
+    :proposals_count => nil,
+    :succeeded_proposals_count => nil,
+    :failed_proposals_count => nil,
+    :votes_count => nil
+  )]}
+  let(:organisation) {mock_model(Organisation,
+    :members => []
+  )}
+
+  before(:each) do
+    view.stub(:current_organisation).and_return(organisation)
+    view.stub(:current_user)
+    view.stub(:co).and_return(organisation)
+    stub_template 'shared/_propose_new_member_form' => "propose_new_member_form"
+  end
+
+  context "when the organisation is pending" do
+    before(:each) do
+      organisation.stub(:pending?).and_return(true)
+      organisation.stub(:proposed?).and_return(false)
+
+      assign(:pending_members, members)
+    end
+
+    it "renders the year of date last logged in" do
+      render
+      rendered.should have_content('1993')
+    end
+  end
+
+  context "when the organisation is active" do
+    before(:each) do
+      organisation.stub(:pending?).and_return(false)
+      organisation.stub(:proposed?).and_return(false)
+
+      assign(:members, members)
+      assign(:pending_members, [])
+    end
+
+    it "renders the year of date last logged in" do
+      render
+      rendered.should have_content('1993')
+    end
+  end
+
+end
