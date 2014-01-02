e9a22e8b5c30b8d8a88a7e85a02c3172a0df3b80
Chris Mear <chris@feedmechocolate.com>
2014-01-02 12:45:45 +0000

    Emails: Move organisation name to subject; in 'From' use OCO instead.

--- spec/mailers/oco_mailer_spec.rb
+++ spec/mailers/oco_mailer_spec.rb
@@ -1,16 +1,28 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
 describe OcoMailer do
   
   class SpecMailer < OcoMailer
-    def notification(organisation_name="One Click Orgs")
+    def notification(organisation_name="My Organisation")
       create_mail(organisation_name, 'bob@example.com', "Example subject")
     end
   end
+
+  it "sets the 'From' name to 'One Click Orgs'" do
+    mail = SpecMailer.notification
+    mail['From'].to_s.should include('One Click Orgs')
+  end
+
+  it "includes the organisation name in the subject" do
+    mail = SpecMailer.notification
+    mail['Subject'].to_s.should == '[My Organisation] Example subject'
+  end
   
-  it "quotes and escapes the 'From:' display name if necessary" do
-    mail = SpecMailer.notification('Bad Company Name"')
-    mail['From'].to_s.should == '"Bad Company Name\\"" <no-reply@oneclickorgs.com>'
+  it "cleans the organisation name in the subject if necessary" do
+    mail = SpecMailer.notification("Bad Company Name\r\n 団体")
+    mail['Subject'].to_s.should == '[Bad Company Name   ] Example subject'
   end
   
 end
