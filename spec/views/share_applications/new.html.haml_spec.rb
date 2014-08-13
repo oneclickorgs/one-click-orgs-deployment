04f60b8ac15c8159b9b8e4535ac1dece1cd1c20a
Chris Mear <chris@feedmechocolate.com>
2014-08-13 15:33:30 +0100

    Warn when trying to apply for shares above the maximum shareholding.

--- spec/views/share_applications/new.html.haml_spec.rb
+++ spec/views/share_applications/new.html.haml_spec.rb
@@ -6,9 +6,11 @@ describe 'share_applications/new' do
     :shares_count => 1
   )}
   let(:share_application) {mock_model(ShareApplication, :amount => nil).as_new_record}
+  let(:organisation) {mock_model(Organisation, maximum_shareholding: 100_000)}
 
   before(:each) do
     view.stub(:current_user).and_return(user)
+    allow(view).to receive(:co).and_return(organisation)
     assign(:share_application, share_application)
   end
 
