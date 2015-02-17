ae00ca7f4b649617858abb4ad6f83528b1616cf4
Chris Mear <chris@feedmechocolate.com>
2015-02-17 12:07:39 +0000

    Prep for version 1.3.10.

--- lib/one_click_orgs/version.rb
+++ lib/one_click_orgs/version.rb
@@ -1,5 +1,5 @@
 module OneClickOrgs
-  VERSION = "1.3.9" unless defined?(::OneClickOrgs::VERSION)
+  VERSION = "1.3.10" unless defined?(::OneClickOrgs::VERSION)
   
   def self.version
     if VERSION =~ /^0/
